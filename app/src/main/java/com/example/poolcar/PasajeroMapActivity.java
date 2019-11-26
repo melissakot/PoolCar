f1eb30455863720c0f970d47392faafbeda02be4
matias lorenzo <emmati1985@gmail.com>
2019-11-26 20:50:45 -0300

    reversados os cambios de api

--- app/src/main/java/com/example/poolcar/PasajeroMapActivity.java
+++ app/src/main/java/com/example/poolcar/PasajeroMapActivity.java
@@ -159,7 +159,7 @@ public class PasajeroMapActivity extends FragmentActivity implements OnMapReadyC
          * environment we recommend using a secure mechanism to manage API keys.
          */
         if (!Places.isInitialized()) {
-            Places.initialize(getApplicationContext(), "@string/google_maps_key"); //"AIzaSyAa66qreGPji58op42F7_ufelBteQxWarg");//"AIzaSyBIRJQawafcAqAjJyDFfgt_Nja6LOCBZEY");
+            Places.initialize(getApplicationContext(), "AIzaSyBIRJQawafcAqAjJyDFfgt_Nja6LOCBZEY"); //"AIzaSyAa66qreGPji58op42F7_ufelBteQxWarg");
             // Create a new Places client instance.
             PlacesClient placesClient = Places.createClient(this);
         }
